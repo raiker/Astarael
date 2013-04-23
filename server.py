36f83dda1b4fed0553c846c730d5e7cd7176976d
Joshua Weberruss <joshua.weberruss@gmail.com>
2013-04-23 23:32:14 +1000

    Changed indentation to spaces

--- server.py
+++ server.py
@@ -8,79 +8,78 @@ import base64
 import hashlib
 
 with open("astarael.db", "rb") as f:
-	(library, image_dict) = pickle.load(f)
+    (library, image_dict) = pickle.load(f)
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("views/library.html")
+    def get(self):
+        self.render("views/library.html")
 
 class LibraryHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.set_header("Content-Type", "application/json")
-		self.write(json.dumps(library))
+    def get(self):
+        self.set_header("Content-Type", "application/json")
+        self.write(json.dumps(library))
 
 class ImageHandler(tornado.web.RequestHandler):
-	def get(self, img_hash):
-		if img_hash in image_dict:
-			res = image_dict[img_hash]
-			self.set_header("Content-Type", res["mimetype"])
-			self.write(res["data"])
-		else:
-			raise tornado.web.HTTPError(404)
+    def get(self, img_hash):
+        if img_hash in image_dict:
+            res = image_dict[img_hash]
+            self.set_header("Content-Type", res["mimetype"])
+            self.write(res["data"])
+        else:
+            raise tornado.web.HTTPError(404)
 
 class StreamHandler(tornado.web.RequestHandler):
-	def get(self, track_id):
-		self.set_header("Content-Type", "audio/mpeg")
+    def get(self, track_id):
+        self.set_header("Content-Type", "audio/mpeg")
 
-		filepath = library[int(track_id)]["filepath"]
+        filepath = library[int(track_id)]["filepath"]
 
-		with open(filepath, "rb") as f:
-			self.write(f.read())
+        with open(filepath, "rb") as f:
+            self.write(f.read())
 
 
 class TrackHandler(tornado.web.RequestHandler):
-	def get(self, track_id):
-		self.set_header("Content-Type", "application/json")
+    def get(self, track_id):
+        self.set_header("Content-Type", "application/json")
 
-		filepath = library[int(track_id)]["filepath"]
-		tag = stagger.read_tag(filepath)
+        filepath = library[int(track_id)]["filepath"]
+        tag = stagger.read_tag(filepath)
 
-		ret_obj = {}
+        ret_obj = {}
 
-		ret_obj["artist"] = tag.artist;
+        ret_obj["artist"] = tag.artist
 
-		# if 'APIC' in tag._frames:
-		# 	apic_frame = tag._frames['APIC']
-		# 	ret_obj["albumart"] = []
-		# 	for art in apic_frame:
-		# 		m = hashlib.md5()
-		# 		m.update(art.data)
-		# 		hashdata = m.digest()
-		# 		hash = base64.b16encode(hashdata).decode("ASCII")
+        # if 'APIC' in tag._frames:
+        # 	apic_frame = tag._frames['APIC']
+        # 	ret_obj["albumart"] = []
+        # 	for art in apic_frame:
+        # 		m = hashlib.md5()
+        # 		m.update(art.data)
+        # 		hashdata = m.digest()
+        # 		hash = base64.b16encode(hashdata).decode("ASCII")
 
-		# 		if not hash in image_dict:
-		# 			image_dict[hash] = {
-		# 				"mimetype": art.mime,
-		# 				"data": art.data
-		# 			}
+        # 		if not hash in image_dict:
+        # 			image_dict[hash] = {
+        # 				"mimetype": art.mime,
+        # 				"data": art.data
+        # 			}
 
-		# 		ret_obj["albumart"].append(hash)
-			
+        # 		ret_obj["albumart"].append(hash)
+            
 
-		self.write(json.dumps(ret_obj))
+        self.write(json.dumps(ret_obj))
 
 settings = {
-	"static_path": os.path.join(os.path.dirname(__file__), "static"),
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
 }
 
-application = tornado.web.Application([
-	(r"/", MainHandler),
-	(r"/api/getlibrary", LibraryHandler),
-	(r"/api/track/(\d+)", TrackHandler),
-	(r"/api/image/([0-9A-F]{32})", ImageHandler),
-	(r"/stream/(\d+)", StreamHandler),
-	(r"/cats", tornado.web.StaticFileHandler, dict(path=settings['static_path'])) #fixme
+application = tornado.web.Application([(r"/", MainHandler),
+    (r"/api/getlibrary", LibraryHandler),
+    (r"/api/track/(\d+)", TrackHandler),
+    (r"/api/image/([0-9A-F]{32})", ImageHandler),
+    (r"/stream/(\d+)", StreamHandler),
+    (r"/cats", tornado.web.StaticFileHandler, dict(path=settings['static_path'])) #fixme
 ], **settings)
 
-application.listen(7742);
+application.listen(17742)
 tornado.ioloop.IOLoop.instance().start()
--- tagreader.py
+++ tagreader.py
@@ -19,50 +19,50 @@ tracks = []
 i = 0
 
 for dirpath, dirnames, filenames in os.walk(basepath):
-	for filename in filenames:
-		if file_matcher.match(filename):
-			filepath = os.path.join(dirpath, filename)
+    for filename in filenames:
+        if file_matcher.match(filename):
+            filepath = os.path.join(dirpath, filename)
 
-			try:
-				tag = stagger.read_tag(filepath)
+            try:
+                tag = stagger.read_tag(filepath)
 
-				albumart = {}
-				if 'APIC' in tag._frames:
-					apic_frame = tag._frames['APIC']
-					for art in apic_frame:
-						m = hashlib.md5()
-						m.update(art.data)
-						hashdata = m.digest()
-						hash = base64.b16encode(hashdata).decode("ASCII")
+                albumart = {}
+                if 'APIC' in tag._frames:
+                    apic_frame = tag._frames['APIC']
+                    for art in apic_frame:
+                        m = hashlib.md5()
+                        m.update(art.data)
+                        hashdata = m.digest()
+                        hash = base64.b16encode(hashdata).decode("ASCII")
 
-						if not hash in image_dict:
-							image_dict[hash] = {
-								"mimetype": art.mime,
-								"data": art.data
-							}
+                        if not hash in image_dict:
+                            image_dict[hash] = {
+                                "mimetype": art.mime,
+                                "data": art.data
+                            }
 
-						albumart[art.type] = hash
+                        albumart[art.type] = hash
 
-				tracks.append({
-					"index": i,
-					"title": tag.title,
-					"track": tag.track,
-					"artist": tag.artist,
-					"album": tag.album,
-					"disc": tag.disc if tag.disc > 0 else 1,
-					"album_artist": tag.album_artist if tag.album_artist != "" else tag.artist,
-					"date": tag.date,
-					"filepath": filepath,
-					"album_art": albumart
-				})
-				i = i + 1
+                tracks.append({
+                    "index": i,
+                    "title": tag.title,
+                    "track": tag.track,
+                    "artist": tag.artist,
+                    "album": tag.album,
+                    "disc": tag.disc if tag.disc > 0 else 1,
+                    "album_artist": tag.album_artist if tag.album_artist != "" else tag.artist,
+                    "date": tag.date,
+                    "filepath": filepath,
+                    "album_art": albumart
+                })
+                i = i + 1
 
-				if i % 100 == 0:
-					print("{0} tracks".format(i))
+                if i % 100 == 0:
+                    print("{0} tracks".format(i))
 
-			except stagger.errors.NoTagError as err:
-				#print("No tag found in {0}".format(filepath))
-				print("No tag found in file")
-				#pass
+            except stagger.errors.NoTagError as err:
+                #print("No tag found in {0}".format(filepath))
+                print("No tag found in file")
+                #pass
 with open("astarael.db","wb") as f:
-	pickle.dump((tracks, image_dict), f)
+    pickle.dump((tracks, image_dict), f)
