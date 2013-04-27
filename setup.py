41d2131592f72d1eb440a240ead6fce91ebc0f6d
Joshua Weberruss <joshua.weberruss@gmail.com>
2013-04-28 01:59:32 +1000

    Tinkering with setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import sys
 from cx_Freeze import setup, Executable
 
-base = None
-
 setup(name = "Astarael",
       version = "0.0.1",
       description = "Astarael Music Player",
-      executables = [Executable("server.py", base=base)])
+      options = {"build_exe": {"include_files": ["views", "static"]}},
+      executables = [Executable("server.py")])
